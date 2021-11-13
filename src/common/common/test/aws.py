829254d81733c6de2522cfbc46a9f09237a8610c
Tom McLaughlin <tom@serverlessops.io>
2021-11-12 19:48:35 -0500

    Add missing CfnStackClient class.
    Used in our integration tests

--- src/common/common/test/aws.py
+++ src/common/common/test/aws.py
@@ -17,3 +17,58 @@ def create_lambda_function_context(function_name: str, object_name: str = 'Lambd
 
     Context = namedtuple(object_name, context_info.keys())
     return Context(*context_info.values())
+
+
+class CfnStackClient(object):
+    '''client for working with a CFN stack'''
+
+    def __init__(self, stack_name):
+        '''init'''
+        self.stack_name = stack_name
+
+        self._cfn_client = boto3.client('cloudformation')
+        self._stack = {}
+        self.refresh()
+
+
+    def refresh(self) -> None:
+        '''Refresh stack info'''
+        self._stack = self._cfn_client.describe_stack_resources(StackName=self.stack_name)
+
+
+    def get_stack_resource_by_logical_id(self, logical_id: str) :
+        '''Get a resource by it's logical ID'''
+        resources = self._stack.get('StackResources', [])
+        r = None
+
+        for _r in resources:
+            if _r.get('LogicalResourceId') == logical_id:
+                r = _r.get('LogicalResourceId')
+                break
+        return r
+
+
+    def get_stack_resource_by_physical_id(self, physical_id: str) -> Union[dict, None]:
+        '''Get a resource by it's logical ID'''
+        resources = self._stack.get('StackResources', [])
+        r = None
+
+        for _res in resources:
+            if _res.get('PhysicalResourceId') == physical_id:
+                r = _res.get('PhysicalResourceId', {})
+                break
+
+        return r
+
+
+    def get_stack_resources_by_type(self, resource_type: str) -> List[Dict[str, Any]]:
+        '''Get resources by resource type'''
+        resources = self._stack.get('StackResources', [])
+        r = []
+
+        for _res in resources:
+            if _res.get('ResourceType') == resource_type:
+                r.append(_res.get('ResourceType'))
+
+        return r
+
