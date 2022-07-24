4b2f3332dbf184fb4a5f832eba183ce5b38a90d1
Tom McLaughlin <tom@serverlessops.io>
2022-07-23 21:55:29 -0400

    refactor IngestS3Event test

--- tests/unit/src/handlers/IngestS3Event/test_function.py
+++ tests/unit/src/handlers/IngestS3Event/test_function.py
@@ -4,62 +4,79 @@
 
 import json
 import os
-
 import pytest
 
 from aws_lambda_powertools.utilities.data_classes import S3Event
-
 from common.test.aws import create_lambda_function_context
 
 import src.handlers.IngestS3Event.function as func
 
-DATA_DIR = './data'
-EVENT_DIR = os.path.join(DATA_DIR, 'events')
-IMAGE_DIR = os.path.join(DATA_DIR, 'images')
-MODEL_DIR = os.path.join(DATA_DIR, 'models')
+FUNCTION_NAME = 'IngestS3event'
 
-### Events
+DATA_DIR = './data'
+COMMON_DATA_DIR = os.path.join(DATA_DIR, 'common')
+FUNC_DATA_DIR = os.path.join(DATA_DIR, 'handlers', FUNCTION_NAME)
+
+EVENT = os.path.join(FUNC_DATA_DIR, 'event.json')
+EVENT_SCHEMA = os.path.join(FUNC_DATA_DIR, 'event.schema.json')
+DATA = [os.path.join(FUNC_DATA_DIR, f) for f in ['data-S3Put.json', 'data-S3Delete.json']]
+DATA_SCHEMA = os.path.join(FUNC_DATA_DIR, 'data.schema.json')
+OUTPUT_SCHEMA = DATA_SCHEMA
+
 @pytest.fixture()
 def context():
     '''context object'''
-    return create_lambda_function_context('IngestS3Event')
+    return create_lambda_function_context(FUNCTION_NAME)
 
 @pytest.fixture()
 def event():
     '''Return a test event'''
-    with open(os.path.join(EVENT_DIR, 'IngestS3Event-event-sns.json')) as f:
+    with open(EVENT) as f:
         return json.load(f)
 
 @pytest.fixture()
 def event_schema():
     '''Return an event schema'''
-    with open(os.path.join(EVENT_DIR, 'lambda-sns-event.schema.json')) as f:
+    with open(EVENT_SCHEMA) as f:
         return json.load(f)
 
 
-@pytest.fixture(params=['IngestS3Event-data-put.json', 'IngestS3Event-data-delete.json'])
-def s3_notification(request):
+@pytest.fixture(params=DATA)
+def data(request):
     '''Return an S3 notification'''
-    with open(os.path.join(EVENT_DIR, request.param)) as f:
+    with open(request.param) as f:
         return json.load(f)
 
 
 @pytest.fixture()
-def s3_notification_schema():
+def data_schema():
     '''Return an S3 notification schema'''
-    with open(os.path.join(EVENT_DIR, 's3-notification.schema.json')) as f:
+    with open(DATA_SCHEMA) as f:
+        return json.load(f)
+
+
+@pytest.fixture()
+def expected_output(data):
+    '''Return an S3 notification'''
+    return data
+
+
+@pytest.fixture()
+def expected_output_schema():
+    '''Return an S3 notification schema'''
+    with open(OUTPUT_SCHEMA) as f:
         return json.load(f)
 
 
 
 ### Tests
-def test_handler(event, s3_notification, context, mocker):
+def test_handler(event, data, expected_output, context, mocker):
     '''Call handler'''
 
-    s3_notification_event = S3Event(s3_notification)
-    event['Records'][0]['Sns']['Message'] = json.dumps(s3_notification_event._data)
+    s3_notification = S3Event(data)
+    event['Records'][0]['Sns']['Message'] = json.dumps(s3_notification._data)
 
     resp = func.handler(event, context)
 
-    assert resp == s3_notification
+    assert resp == expected_output
 
